1c4af8845a79177ed0115b12770fbea8979cb20b
Ian Thompson <ianthompson@nicelion.com>
2023-07-07 11:55:46 -0400

    remove import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,60 +1,51 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { TeachableMachine, TeachableWrapper } from '../util/TM';
+	import {  TeachableWrapper } from '../util/TM';
 
-
-	
 	const loadFaces = () => {
 		return new Promise((resolve, reject) => {
-			let faces: [HTMLImageElement] = []
+			let faces: [HTMLImageElement] = [];
 
 			for (let index = 0; index < 10; index++) {
-				let img = document.createElement('img')
-				img.src = `/face/${index}.png`
-				faces = [...faces, img]
+				let img = document.createElement('img');
+				img.src = `/face/${index}.png`;
+				faces = [...faces, img];
 			}
 
-			resolve(faces)
+			resolve(faces);
 		});
-	}
+	};
 
 	const loadHands = () => {
 		return new Promise((resolve, reject) => {
-			let hands: [HTMLImageElement] = []
+			let hands: [HTMLImageElement] = [];
 
 			for (let index = 0; index < 10; index++) {
-				let img = document.createElement('img')
-				img.src = `/hand/${index}.png`
-				hands = [...hands, img]
+				let img = document.createElement('img');
+				img.src = `/hand/${index}.png`;
+				hands = [...hands, img];
 			}
 
-			resolve(hands)
+			resolve(hands);
 		});
-	}
+	};
 
 	onMount(async () => {
-
-
 		/**
 		 * Because loading these images takes time and JS is dumb, we need to wait
-		 * for the load functions to "resolve" or complete. These are known as promises 
-		 * and you can read more about it online. They are a little complicated and more 
+		 * for the load functions to "resolve" or complete. These are known as promises
+		 * and you can read more about it online. They are a little complicated and more
 		 * advanced feature, but essentially it allows us to create synchronous code.
-		 * 
-		 * Both of the above functions return arrays of HTMLImageElements which are expected in 
+		 *
+		 * Both of the above functions return arrays of HTMLImageElements which are expected in
 		 * training.
 		 */
 
-		let faces = await loadFaces()
-		let hands = await loadHands()
-
+		let faces = await loadFaces();
+		let hands = await loadHands();
 
 		TeachableWrapper.initTeachable(faces, hands);
-
-
 	});
 </script>
 
 <h1 class="text-3xl font-bold underline">TeachableMachine Svelte-wrapper</h1>
-
-
